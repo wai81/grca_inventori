b397eb7484e40ee9efecc07efb0ffbe129a0f764
wai81 <50743966+wai81@users.noreply.github.com>
2026-02-20 13:19:45 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+
+    'inventory'
 ]
 
 MIDDLEWARE = [
@@ -116,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
